f765a3c83aadce3edb4680d9ca61b922be03319d
Luka <luka.djordjevic@elfak.rs>
2026-02-16 12:02:34 +0100

    komunikacija initial

---
+++ Communications/Communications/StartForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Communications
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            //pribavljanje imena iz baze i gameid iz baze
+
+            int numUsers = 4;
+            for(int i= 0;i<numUsers;i++)
+            {
+                ChatForm chatForm = new ChatForm("user"+i, "game");
+                chatForm.Show();
+            }
+        }
+    }
+}
